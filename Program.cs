4d2ec193f1202b25d7c96868cccabc0ba7c35ad5
George <georgest@mail.ru>
2023-07-19 14:04:06 +0300

    Всё выполнено

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿
-﻿string[] arr1 = new string[8] {"hello", "2", "world", ":-)", "1234", "computer science", "Russia", "-2"};
+String[] arr1 = {"hello", "2", "world", ":-)", "1234", "computer science", "Russia", "-2"};
 string[] arr2 = new string[arr1.Length];
 
 void Process(string[] arr1, string[] arr2){
     int n = 0;
     for (int i = 0; i < arr1.Length; i++){
-    if(arr1[i].Length <= 3){
-        arr2[n] = arr1[i];
-        n++;
+        if(arr1[i].Length <= 3){
+          arr2[n] = arr1[i];
+         n++;
         }
     }
+    Array.Resize(ref arr2, n);
 }
 
 void PrintArray(string[] arr){
