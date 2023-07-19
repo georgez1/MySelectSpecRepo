34d90d5b147faddb3090324949d997cabbed6e4b
George <georgest@mail.ru>
2023-07-19 13:50:42 +0300

    Добавлена программа на C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+﻿string[] arr1 = new string[8] {"hello", "2", "world", ":-)", "1234", "computer science", "Russia", "-2"};
+string[] arr2 = new string[arr1.Length];
+
+void Process(string[] arr1, string[] arr2){
+    int n = 0;
+    for (int i = 0; i < arr1.Length; i++){
+    if(arr1[i].Length <= 3){
+        arr2[n] = arr1[i];
+        n++;
+        }
+    }
+}
+
+void PrintArray(string[] arr){
+    for (int i = 0; i < arr.Length; i++){
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr1);
+Process(arr1, arr2);
+PrintArray(arr2);
